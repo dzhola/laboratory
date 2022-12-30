0166601696bfa25d9a612512a806a4f335c503f2
Kristina <121455314+dzhola@users.noreply.github.com>
2022-12-30 09:34:49 +0200

    Lab4-Massive

---
+++ Lab4-Massive/Massive/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Введіть кількість елементів масиву:\t");
+            int elementsCount = Convert.ToInt32(Console.ReadLine());
+            int[] myArray = new int[elementsCount];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"\nВведіть елемент масиву під індексом {i}:\t");
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nВивід масиву:");
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write(myArray[i] + "  ");
+            }
+            Console.WriteLine();
+
+
+
+            // int[] myArray = { 822, 45, 632, 14, 25, 89 };
+            // for (int i = 0; i < myArray.Length; i++)
+            // {
+            //     Console.Write(myArray[i] + "  ");
+            // }
+            // Console.WriteLine();
+            // Метод розвороту масива
+            void ReversalMassiv(int[] Arr)
+            {
+                int length = Arr.Length;
+                if (Arr.Length % 2 == 0)
+                {
+                    length = length / 2;
+                }
+                else
+                    length = (length - 1) / 2;
+
+                for (int i = 0; i < length; i++)
+                {
+                    int a = Arr[i];
+                    Arr[i] = Arr[Arr.Length - 1 - i];
+                    Arr[Arr.Length - 1 - i] = a;
+                }
+                Console.WriteLine("\nВивід розвернутого масиву:");
+
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    Console.Write(Arr[i] + "  ");
+                }
+                Console.WriteLine();
+            }
+            ReversalMassiv(myArray);
+
+            Console.WriteLine();
+
+            // Метод знаходження суми не(парних) елементів масиву 
+
+            void EvenFind(int[] Array)
+            {
+                int summa = 0;
+                for (int i = 0; i < Array.Length; i++)
+                {
+                    if (Array[i] % 2 == 0)
+                    {
+                        summa = summa + Array[i];
+                    }
+                }
+                Console.WriteLine($"Сума парних елементів: {summa}");
+            }
+            EvenFind(myArray);
+
+            // Найбільший(найменший) елемент масиву
+
+            void Less_moreMassiv(int[] Array)
+            {
+                int max = Array[0];
+                for (int i = 1; i < Array.Length; i++)
+                {
+                    if (Array[i] > Array[0])
+                    {
+                        max = Array[i];
+                    }
+
+                }
+                Console.WriteLine($"Найбільший елемент масиву: {max}");
+            }
+            Less_moreMassiv(myArray);
+
+        }
+    }
+}
